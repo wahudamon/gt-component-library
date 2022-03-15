a2e1e4ef2f10e8da0d3c89dde6c1ab6707742a8d
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-03-15 11:11:32 +0700

    feat: setup unit test

---
+++ tests/unit/button.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils";
+import Button from "@/lib-components/Button.vue";
+
+describe("Button.vue", () => {
+  it("renders the props when passed", () => {
+    const label = "Hello World!";
+    const primary = true;
+    const size = "medium";
+    const wrapper = shallowMount(Button, {
+      propsData: { label, primary, size },
+    });
+    expect(wrapper.props().label).toBe(label);
+    expect(wrapper.props().primary).toBe(primary);
+    expect(wrapper.props().size).toBe(size);
+  });
+});
