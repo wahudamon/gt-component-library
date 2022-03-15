6a0df3255e64e8fce4aff1571e6757ae4cf6b0ab
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-03-15 10:37:55 +0700

    init vue sfc rollup starter

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { GamatechnoVueComponentLibrarySample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  GamatechnoVueComponentLibrarySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <gamatechno-vue-component-library-sample />
+  </div>
+</template>
