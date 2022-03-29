24b21af6939c12f4fb8fa89ebc1c1fe3be8580ca
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-03-29 10:35:41 +0700

    style: convert corner radius from design to code

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,30 @@
 <script>
 import Vue from "vue";
-import { Button } from "@/entry.esm";
+// import { Button } from "@/entry.esm";
 
 export default Vue.extend({
   name: "ServeDev",
-  components: {
-    Button,
-  },
+  // components: {
+  //   Button,
+  // },
 });
 </script>
 
 <template>
   <div id="app">
-    <Button primary size="small" label="Hello World!" />
+    <!-- <Button primary size="small" label="Hello World!" /> -->
+    <div class="sample-box cr--lg bg--secondary-teal"></div>
   </div>
 </template>
+
+<style lang="scss">
+@import "../src/styles/color";
+@import "../src/styles/corner-radius";
+
+.sample-box {
+  margin-top: 50px;
+  margin-left: 530px;
+  width: 400px;
+  height: 200px;
+}
+</style>
