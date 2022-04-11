6514a4de9e4b6a423072acad129abb169fc9d742
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-04-11 14:34:31 +0700

    feat: create checkbox and radio button component examples

---
+++ dev/examples/CheckboxExample.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Show Checkboxes</h1>
+    <form>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+      <label class="ml--2" for="vehicle1"> I have a bike</label><br />
+      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+      <label class="ml--2" for="vehicle2"> I have a car</label><br />
+      <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat" />
+      <label class="ml--2" for="vehicle3"> I have a boat</label><br /><br />
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "checkbox-example",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../css/typography";
+@import "../../css/margin";
+</style>
--- dev/serve.vue
+++ dev/serve.vue
@@ -4,23 +4,33 @@ import Vue from "vue";
 // import GridSystem from "./examples/GridSystem.vue";
 // import Typography from "./examples/Typography.vue";
 // import CornerRadius from "./examples/CornerRadius.vue";
+import CheckboxExample from "./examples/CheckboxExample.vue";
+import RadioButtonExample from "./examples/RadioButtonExample.vue";
 
 export default Vue.extend({
   name: "ServeDev",
   components: {
+    CheckboxExample,
+    RadioButtonExample,
+    // Button,
     // GridSystem,
     // Typography,
     // CornerRadius,
-    // Button,
   },
 });
 </script>
 
 <template>
   <div id="app">
+    <checkbox-example class="ml--20 mt--20" />
+    <radio-button-example class="ml--20 mt--10" />
     <!-- <typography />
     <grid-system />
     <corner-radius /> -->
     <!-- <Button primary size="small" label="Hello World!" /> -->
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "../css/margin";
+</style>
