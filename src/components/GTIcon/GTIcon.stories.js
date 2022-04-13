2aa34fa440d4f9a25a47e531c87d147d0a70f268
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-04-13 10:41:54 +0700

    feat: add communication regular icons

--- src/components/GTIcon/GTIcon.stories.js
+++ src/components/GTIcon/GTIcon.stories.js
@@ -34,30 +34,9 @@ const Template = (args) => ({
 
 export const Primary = Template.bind({});
 Primary.args = {
-  name: "gt-add-block",
+  name: "gt-address-book-r",
   size: "40",
 };
-
-export const CommunicationSolid = (args) => ({
-  props: Object.keys(args),
-  components: { GTIcon },
-  data: () => ({
-    icons: availableIconsObj,
-  }),
-  template: `
-    <div style="display: flex; flex-wrap: wrap; justify-content: space-between;">
-      <div
-        v-for="(icon, index) in icons"
-        v-if="icon.type == 'comm-solid'"
-        :key="index"
-        style="text-align: center; padding: 2rem"
-      >
-        <GTIcon :name="icon.name" size="40" />
-        <p> {{ icon.name }} </p>
-      </div>
-    </div>
-  `,
-});
 
 export const CommunicationRegular = (args) => ({
   props: Object.keys(args),
@@ -66,7 +45,7 @@ export const CommunicationRegular = (args) => ({
     icons: availableIconsObj,
   }),
   template: `
-    <div style="display: flex; flex-wrap: wrap; justify-content: space-between;">
+    <div style="display: grid; grid-template-columns: repeat(4, 1fr);">
       <div
         v-for="(icon, index) in icons"
         v-if="icon.type == 'comm-regular'"
@@ -79,3 +58,24 @@ export const CommunicationRegular = (args) => ({
     </div>
   `,
 });
+
+// export const CommunicationSolid = (args) => ({
+//   props: Object.keys(args),
+//   components: { GTIcon },
+//   data: () => ({
+//     icons: availableIconsObj,
+//   }),
+//   template: `
+//     <div style="display: flex; flex-wrap: wrap; justify-content: space-between;">
+//       <div
+//         v-for="(icon, index) in icons"
+//         v-if="icon.type == 'comm-solid'"
+//         :key="index"
+//         style="text-align: center; padding: 2rem"
+//       >
+//         <GTIcon :name="icon.name" size="40" />
+//         <p> {{ icon.name }} </p>
+//       </div>
+//     </div>
+//   `,
+// });
