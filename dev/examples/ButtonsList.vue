cbf802cd4376f1810668d5538fdb6412312b6e95
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-04-18 14:54:21 +0700

    feat: create tertiary type button component

--- dev/examples/ButtonsList.vue
+++ dev/examples/ButtonsList.vue
@@ -12,7 +12,7 @@ export default Vue.extend({
 
 <template>
   <div>
-    <p class="mt--10 ml--20 playfair playfair--h3">Primary Buttons</p>
+    <p class="mt--10 ml--20 playfair playfair--h5">Primary Buttons</p>
     <div class="mt--5 mb--15 grid--container">
       <div class="grid--row">
         <div class="grid--col-lg-12">
@@ -167,7 +167,7 @@ export default Vue.extend({
         </div>
       </div>
     </div>
-    <p class="mt--10 ml--20 playfair playfair--h3">Secondary Buttons</p>
+    <p class="mt--10 ml--20 playfair playfair--h5">Secondary Buttons</p>
     <div class="mt--5 mb--15 grid--container">
       <div class="grid--row">
         <div class="grid--col-lg-12">
@@ -222,6 +222,269 @@ export default Vue.extend({
             variant="red-200"
             isLoading
             size="sm"
+          />
+        </div>
+        <div class="grid--col-lg-12">
+          <GTButton
+            type="secondary"
+            variant="red-200"
+            size="md"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="secondary"
+            size="md"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="secondary"
+            variant="red-200"
+            size="md"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="secondary"
+            size="md"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            type="secondary"
+            variant="red-200"
+            size="md"
+            isPrependIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="secondary"
+            variant="red-200"
+            size="md"
+            isAppendIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="secondary"
+            variant="red-200"
+            isLoading
+            size="md"
+          />
+        </div>
+        <div class="grid--col-lg-12">
+          <GTButton
+            type="secondary"
+            variant="red-200"
+            size="lg"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="secondary"
+            size="lg"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="secondary"
+            variant="red-200"
+            size="lg"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="secondary"
+            size="lg"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            type="secondary"
+            variant="red-200"
+            size="lg"
+            isPrependIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+        </div>
+        <div class="grid--col-lg-12">
+          <GTButton
+            type="secondary"
+            variant="red-200"
+            size="lg"
+            isAppendIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="secondary"
+            variant="red-200"
+            isLoading
+            size="lg"
+          />
+        </div>
+      </div>
+    </div>
+    <p class="mt--10 ml--20 playfair playfair--h5">Tertiary Buttons</p>
+    <div class="mt--5 mb--15 grid--container">
+      <div class="grid--row">
+        <div class="grid--col-lg-12">
+          <GTButton type="link" variant="red-200" size="sm" text="Button" />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="link"
+            size="sm"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="sm"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="link"
+            size="sm"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="sm"
+            isPrependIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="sm"
+            isAppendIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            isLoading
+            size="sm"
+          />
+        </div>
+        <div class="grid--col-lg-12">
+          <GTButton type="link" variant="red-200" size="md" text="Button" />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="link"
+            size="md"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="md"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="link"
+            size="md"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="md"
+            isPrependIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="md"
+            isAppendIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            isLoading
+            size="md"
+          />
+        </div>
+        <div class="grid--col-lg-12">
+          <GTButton type="link" variant="red-200" size="lg" text="Button" />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="link"
+            size="lg"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="lg"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            isDisabled
+            type="link"
+            size="lg"
+            icon="gt-paper-plane-s"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="lg"
+            isPrependIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+        </div>
+        <div class="grid--col-lg-12">
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            size="lg"
+            isAppendIcon
+            icon="gt-paper-plane-s"
+            text="Button"
+          />
+          <GTButton
+            class="ml--5"
+            type="link"
+            variant="red-200"
+            isLoading
+            size="lg"
           />
         </div>
       </div>
