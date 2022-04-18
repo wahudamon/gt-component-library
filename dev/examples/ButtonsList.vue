51da9190e323d61b5272e654fe0da03d2c9bb0da
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-04-18 09:47:27 +0700

    feat: create button active event

--- dev/examples/ButtonsList.vue
+++ dev/examples/ButtonsList.vue
@@ -16,29 +16,18 @@ export default Vue.extend({
     <div class="mt--5 grid--container">
       <div class="grid--row">
         <div class="grid--col-lg-12">
-          <GTButton class="bg--red-200" primary size="sm" text="Button" />
+          <GTButton primary size="sm" text="Button" />
+          <GTButton class="ml--5" isDisabled primary size="sm" text="Button" />
+          <GTButton class="ml--5" primary size="sm" icon="gt-paper-plane-s" />
           <GTButton
-            class="ml--5 bg--red-200"
-            isDisabled
-            primary
-            size="sm"
-            text="Button"
-          />
-          <GTButton
-            class="ml--5 bg--red-200"
-            primary
-            size="sm"
-            icon="gt-paper-plane-s"
-          />
-          <GTButton
-            class="ml--5 bg--red-200"
+            class="ml--5"
             isDisabled
             primary
             size="sm"
             icon="gt-paper-plane-s"
           />
           <GTButton
-            class="ml--5 bg--red-200"
+            class="ml--5"
             primary
             size="sm"
             isPrependIcon
@@ -46,39 +35,28 @@ export default Vue.extend({
             text="Button"
           />
           <GTButton
-            class="ml--5 bg--red-200"
+            class="ml--5"
             primary
             size="sm"
             isAppendIcon
             icon="gt-paper-plane-s"
             text="Button"
           />
-          <GTButton class="ml--5 bg--red-200" primary isLoading size="sm" />
+          <GTButton class="ml--5" primary isLoading size="sm" />
         </div>
         <div class="grid--col-lg-12">
-          <GTButton class="bg--red-200" primary size="md" text="Button" />
+          <GTButton primary size="md" text="Button" />
+          <GTButton class="ml--5" isDisabled primary size="md" text="Button" />
+          <GTButton class="ml--5" primary size="md" icon="gt-paper-plane-s" />
           <GTButton
-            class="ml--5 bg--red-200"
-            isDisabled
-            primary
-            size="md"
-            text="Button"
-          />
-          <GTButton
-            class="ml--5 bg--red-200"
-            primary
-            size="md"
-            icon="gt-paper-plane-s"
-          />
-          <GTButton
-            class="ml--5 bg--red-200"
+            class="ml--5"
             isDisabled
             primary
             size="md"
             icon="gt-paper-plane-s"
           />
           <GTButton
-            class="ml--5 bg--red-200"
+            class="ml--5"
             primary
             size="md"
             isPrependIcon
@@ -86,39 +64,28 @@ export default Vue.extend({
             text="Button"
           />
           <GTButton
-            class="ml--5 bg--red-200"
+            class="ml--5"
             primary
             size="md"
             isAppendIcon
             icon="gt-paper-plane-s"
             text="Button"
           />
-          <GTButton class="ml--5 bg--red-200" primary isLoading size="md" />
+          <GTButton class="ml--5" primary isLoading size="md" />
         </div>
         <div class="grid--col-lg-12">
-          <GTButton class="bg--red-200" primary size="lg" text="Button" />
+          <GTButton primary size="lg" text="Button" />
+          <GTButton class="ml--5" isDisabled primary size="lg" text="Button" />
+          <GTButton class="ml--5" primary size="lg" icon="gt-paper-plane-s" />
           <GTButton
-            class="ml--5 bg--red-200"
-            isDisabled
-            primary
-            size="lg"
-            text="Button"
-          />
-          <GTButton
-            class="ml--5 bg--red-200"
-            primary
-            size="lg"
-            icon="gt-paper-plane-s"
-          />
-          <GTButton
-            class="ml--5 bg--red-200"
+            class="ml--5"
             isDisabled
             primary
             size="lg"
             icon="gt-paper-plane-s"
           />
           <GTButton
-            class="ml--5 bg--red-200"
+            class="ml--5"
             primary
             size="lg"
             isPrependIcon
@@ -129,14 +96,13 @@ export default Vue.extend({
         <div class="grid--row">
           <div class="grid--col-lg-12">
             <GTButton
-              class="bg--red-200"
               primary
               size="lg"
               isAppendIcon
               icon="gt-paper-plane-s"
               text="Button"
             />
-            <GTButton class="ml--5 bg--red-200" primary isLoading size="lg" />
+            <GTButton class="ml--5" primary isLoading size="lg" />
           </div>
         </div>
       </div>
