e33fedba2ee88ce7ffde3a2b7261be69ba088257
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-04-19 10:57:03 +0700

    feat: set label font family and checkmark border

--- src/components/GTCheckbox/GTCheckbox.vue
+++ src/components/GTCheckbox/GTCheckbox.vue
@@ -1,6 +1,6 @@
 <template>
-  <label class="container">
-    Text
+  <label :class="classes">
+    Label
     <input type="checkbox" />
     <span class="checkmark"></span>
   </label>
@@ -9,6 +9,40 @@
 <script>
 export default {
   name: "GTCheckbox",
+
+  props: {
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: "primary",
+      validator: function (value) {
+        return ["primary", "secondary", "link"].indexOf(value) !== -1;
+      },
+    },
+    size: {
+      type: String,
+      default: "md",
+      validator: function (value) {
+        return ["sm", "md", "lg"].indexOf(value) !== -1;
+      },
+    },
+    // should be deleted!
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    classes() {
+      return {
+        "gt-checkbox": true,
+      };
+    },
+  },
 };
 </script>
 
