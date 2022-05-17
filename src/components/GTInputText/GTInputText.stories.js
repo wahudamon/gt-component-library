ca1865656205ed7d6d38db40e350f00a8bd7c49d
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-17 23:20:08 +0700

    docs: create input text props list and stories

--- src/components/GTInputText/GTInputText.stories.js
+++ src/components/GTInputText/GTInputText.stories.js
@@ -12,12 +12,16 @@ export default {
       control: { type: "select" },
       options: ["sm", "md", "lg"],
     },
+    variant: {
+      control: { type: "select" },
+      options: ["outline", "horizontal", "vertical"],
+    },
   },
   parameters: {
     viewMode: "docs",
     docs: {
       description: {
-        component: "Lorem ipsum dolor sir amet.",
+        component: "Input text defines a single-line text field.",
       },
     },
   },
@@ -34,5 +38,84 @@ Primary.args = {
   variant: "outline",
   size: "sm",
   length: "sm",
-  label: "Username",
+  label: "Label",
+};
+
+export const SmallLength = Template.bind({});
+SmallLength.args = {
+  variant: "outline",
+  size: "sm",
+  length: "sm",
+  label: "Label",
+};
+
+export const MediumLength = Template.bind({});
+MediumLength.args = {
+  variant: "outline",
+  size: "sm",
+  length: "md",
+  label: "Label",
+};
+
+export const LargeLength = Template.bind({});
+LargeLength.args = {
+  variant: "outline",
+  size: "sm",
+  length: "lg",
+  label: "Label",
+};
+
+export const HorizontalLabel = Template.bind({});
+HorizontalLabel.args = {
+  variant: "horizontal",
+  size: "sm",
+  length: "sm",
+  label: "Label",
+  placeholder: "placeholder",
+};
+
+export const VerticalLabel = Template.bind({});
+VerticalLabel.args = {
+  variant: "vertical",
+  size: "sm",
+  length: "sm",
+  label: "Label",
+  placeholder: "placeholder",
+};
+
+export const HelperText = Template.bind({});
+HelperText.args = {
+  variant: "outline",
+  size: "sm",
+  length: "sm",
+  label: "Label",
+  helperText: "Helper Text",
+};
+
+export const LeadingIcon = Template.bind({});
+LeadingIcon.args = {
+  variant: "outline",
+  size: "sm",
+  length: "sm",
+  label: "Label",
+  leadingIcon: "gt-paper-plane-s",
+};
+
+export const TrailingIcon = Template.bind({});
+TrailingIcon.args = {
+  variant: "outline",
+  size: "sm",
+  length: "sm",
+  label: "Label",
+  trailingIcon: "gt-paper-plane-s",
+};
+
+export const LeadingAndTrailingIcon = Template.bind({});
+LeadingAndTrailingIcon.args = {
+  variant: "outline",
+  size: "sm",
+  length: "sm",
+  label: "Label",
+  leadingIcon: "gt-paper-plane-s",
+  trailingIcon: "gt-paper-plane-s",
 };
