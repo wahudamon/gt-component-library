9f779d650b91aacaafa4930a867572ddf8415897
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-17 11:34:32 +0700

    feat: setup input area component files

---
+++ src/components/GTInputArea/GTInputArea.stories.js
@@ -0,0 +1,36 @@
+// import GTInputArea from "./GTInputArea.vue";
+
+// export default {
+//   title: "Components/GTInputArea",
+//   component: GTInputArea,
+//   argTypes: {
+//     size: {
+//       control: { type: "select" },
+//       options: ["sm", "md", "lg"],
+//     },
+//   },
+//   parameters: {
+//     viewMode: "docs",
+//     docs: {
+//       description: {
+//         component:
+//           "Radio buttons are normally presented in radio groups (a collection of radio buttons describing a set of related options). Only one radio button in a group can be selected at the same time.",
+//       },
+//     },
+//   },
+// };
+
+// const Template = (args, { argTypes }) => ({
+//   props: Object.keys(argTypes),
+//   components: { GTInputArea },
+//   template: '<GTInputArea v-bind="$props" />',
+// });
+
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   type: "primary",
+//   text: "Text",
+//   subtext: "This is subtext",
+//   size: "md",
+//   isDisabled: false,
+// };
---
+++ src/components/GTInputArea/GTInputArea.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script>
+export default {
+  name: "GTInputArea",
+};
+</script>
+
+<style lang="scss">
+@import "./GTInputArea.scss";
+</style>
