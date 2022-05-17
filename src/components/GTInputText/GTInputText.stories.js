2bfd77fe530d2c029b1a6f6fd01296de7b6ad16d
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-17 20:30:09 +0700

    docs: create base version input text docs

--- src/components/GTInputText/GTInputText.stories.js
+++ src/components/GTInputText/GTInputText.stories.js
@@ -1,36 +1,38 @@
-// import GTInputText from "./GTInputText.vue";
+import GTInputText from "./GTInputText.vue";
 
-// export default {
-//   title: "Components/GTInputText",
-//   component: GTInputText,
-//   argTypes: {
-//     size: {
-//       control: { type: "select" },
-//       options: ["sm", "md", "lg"],
-//     },
-//   },
-//   parameters: {
-//     viewMode: "docs",
-//     docs: {
-//       description: {
-//         component:
-//           "Radio buttons are normally presented in radio groups (a collection of radio buttons describing a set of related options). Only one radio button in a group can be selected at the same time.",
-//       },
-//     },
-//   },
-// };
+export default {
+  title: "Components/GTInputText",
+  component: GTInputText,
+  argTypes: {
+    size: {
+      control: { type: "select" },
+      options: ["sm", "md"],
+    },
+    length: {
+      control: { type: "select" },
+      options: ["sm", "md", "lg"],
+    },
+  },
+  parameters: {
+    viewMode: "docs",
+    docs: {
+      description: {
+        component: "Lorem ipsum dolor sir amet.",
+      },
+    },
+  },
+};
 
-// const Template = (args, { argTypes }) => ({
-//   props: Object.keys(argTypes),
-//   components: { GTInputText },
-//   template: '<GTInputText v-bind="$props" />',
-// });
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { GTInputText },
+  template: '<GTInputText v-bind="$props" />',
+});
 
-// export const Primary = Template.bind({});
-// Primary.args = {
-//   type: "primary",
-//   text: "Text",
-//   subtext: "This is subtext",
-//   size: "md",
-//   isDisabled: false,
-// };
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: "outline",
+  size: "sm",
+  length: "sm",
+  label: "Username",
+};
