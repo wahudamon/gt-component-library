7152ff8e3ca0e82c768b586a4bbbdc987a406075
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-18 11:16:24 +0700

    feat: setup breadcrumb component files

---
+++ src/components/GTBreadcrumb/GTBreadcrumb.stories.js
@@ -0,0 +1,42 @@
+// import GTBreadcrumb from "./GTBreadcrumb.vue";
+
+// export default {
+//   title: "Components/GTBreadcrumb",
+//   component: GTBreadcrumb,
+//   argTypes: {
+//     size: {
+//       control: { type: "select" },
+//       options: ["sm", "md"],
+//     },
+//     length: {
+//       control: { type: "select" },
+//       options: ["sm", "md", "lg"],
+//     },
+//     variant: {
+//       control: { type: "select" },
+//       options: ["outline", "horizontal", "vertical"],
+//     },
+//   },
+//   parameters: {
+//     viewMode: "docs",
+//     docs: {
+//       description: {
+//         component: "Input text defines a single-line text field.",
+//       },
+//     },
+//   },
+// };
+
+// const Template = (args, { argTypes }) => ({
+//   props: Object.keys(argTypes),
+//   components: { GTInputText },
+//   template: '<GTInputText v-bind="$props" />',
+// });
+
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   variant: "outline",
+//   size: "sm",
+//   length: "sm",
+//   label: "Label",
+// };
---
+++ src/components/GTBreadcrumb/GTBreadcrumb.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script>
+export default {
+  name: "GTBreadcrumb",
+};
+</script>
+
+<style lang="scss">
+@import "./GTBreadcrumb.scss";
+</style>
