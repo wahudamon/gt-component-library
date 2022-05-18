1f4047c8d3791d921a4864ebd06d4998b0e9559a
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-18 21:23:03 +0700

    feat: add house chimpney solid icon

--- src/components/GTIcon/GTIcon.stories.js
+++ src/components/GTIcon/GTIcon.stories.js
@@ -105,3 +105,24 @@ export const NavigationSolid = (args) => ({
     </div>
   `,
 });
+
+export const Buildings = (args) => ({
+  props: Object.keys(args),
+  components: { GTIcon },
+  data: () => ({
+    icons: availableIconsObj,
+  }),
+  template: `
+    <div style="display: grid; grid-template-columns: repeat(4, 1fr);">
+      <div
+        v-for="(icon, index) in icons"
+        v-if="icon.type == 'buildings'"
+        :key="index"
+        style="text-align: center; padding: 2rem"
+      >
+        <GTIcon :name="icon.name" size="lg" />
+        <p> {{ icon.name }} </p>
+      </div>
+    </div>
+  `,
+});
