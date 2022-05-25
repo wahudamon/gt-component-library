d79c4b0b33c8ecf91a4a80f80b9167780a2c1e19
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-25 14:02:42 +0700

    feat: create upper value for slider component

--- src/components/GTSlider/GTSlider.vue
+++ src/components/GTSlider/GTSlider.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="gt-slider__container">
+    <span id="gt-slider__value-label" class="gt-slider__value-label">
+      {{ sliderVal }}
+    </span>
     <input
       v-model="sliderVal"
       class="gt-slider__field"
@@ -38,13 +41,16 @@ export default {
     return {
       sliderVal: "50",
       sliderElement: null,
+      valueLabelElement: null,
     };
   },
 
   mounted() {
     this.sliderElement = document.getElementById("gt-slider__field");
+    this.valueLabelElement = document.getElementById("gt-slider__value-label");
 
     this.initialTrackFill(this.sliderElement);
+    this.initialValueLabelPosition(this.sliderElement);
   },
 
   methods: {
@@ -59,6 +65,24 @@ export default {
       }
     },
 
+    initialValueLabelPosition(slider) {
+      if (slider) {
+        const min = slider.min;
+        const max = slider.max;
+        const value = slider.value;
+
+        this.valueLabelElement.innerHTML = value;
+
+        let valueLabelPosition = value / max;
+
+        if (value === min) {
+          this.valueLabelElement.style.left = 0;
+        } else {
+          this.valueLabelElement.style.left = valueLabelPosition * 22.5 + "rem";
+        }
+      }
+    },
+
     handleInputChange(e) {
       let target = e.target;
 
@@ -69,6 +93,16 @@ export default {
 
         target.style.backgroundSize =
           ((value - min) * 100) / (max - min) + "% 100%";
+
+        this.valueLabelElement.innerHTML = value;
+
+        let valueLabelPosition = value / max;
+
+        if (value === min) {
+          this.valueLabelElement.style.left = 0;
+        } else {
+          this.valueLabelElement.style.left = valueLabelPosition * 22.5 + "rem";
+        }
       }
     },
   },
