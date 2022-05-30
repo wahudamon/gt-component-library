de0dae899b11ecdc8a63e9a696812ba9cd9397d9
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-30 16:26:28 +0700

    feat: add combobox reference from internet

--- src/components/GTCombobox/GTCombobox.vue
+++ src/components/GTCombobox/GTCombobox.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Hello World!</div>
+  <div class="combobox">
+    <select name="languages" id="lang">
+      <option value="select">Placeholder</option>
+      <option value="javascript">JavaScript</option>
+      <option value="php">PHP</option>
+      <option value="java">Java</option>
+      <option value="golang">Golang</option>
+      <option value="python">Python</option>
+      <option value="c#">C#</option>
+      <option value="C++">C++</option>
+      <option value="erlang">Erlang</option>
+    </select>
+  </div>
 </template>
 
 <script>
