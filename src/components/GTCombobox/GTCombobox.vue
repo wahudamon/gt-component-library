7794033057a81a947044733e0a7f2a6b196ea02c
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-05-31 10:29:20 +0700

    feat: create small and medium size of combobox component

--- src/components/GTCombobox/GTCombobox.vue
+++ src/components/GTCombobox/GTCombobox.vue
@@ -1,22 +1,40 @@
 <template>
-  <div class="combobox">
-    <select name="languages" id="lang">
-      <option value="select">Placeholder</option>
-      <option value="javascript">JavaScript</option>
-      <option value="php">PHP</option>
-      <option value="java">Java</option>
-      <option value="golang">Golang</option>
-      <option value="python">Python</option>
-      <option value="c#">C#</option>
-      <option value="C++">C++</option>
-      <option value="erlang">Erlang</option>
-    </select>
-  </div>
+  <select :class="classes">
+    <option value="select">Select a car...</option>
+    <option value="lamborghini">Lamborghini</option>
+    <option value="ferrari">Ferrari</option>
+    <option value="mitsubishi">Mitsubishi</option>
+    <option value="toyota">Toyota</option>
+    <option value="suzuki">Suzuki</option>
+    <option value="supra">Supra</option>
+    <option value="chevrolet">Chevrolet</option>
+    <option value="pulsar">Pulsar</option>
+  </select>
 </template>
 
 <script>
 export default {
   name: "GTCombobox",
+
+  props: {
+    size: {
+      type: String,
+      default: "md",
+      validator: function (value) {
+        return ["sm", "md"].indexOf(value) !== -1;
+      },
+    },
+  },
+
+  computed: {
+    classes() {
+      return {
+        "gt-combobox": true,
+        "form-select": true,
+        "form-select-sm": this.size && this.size === "sm",
+      };
+    },
+  },
 };
 </script>
 
