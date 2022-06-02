ac0c08046d2e8552cce8afea2c4d8fe37dc64b96
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-06-02 15:36:30 +0700

    feat: update combobox base version

--- src/components/GTCombobox/GTCombobox.vue
+++ src/components/GTCombobox/GTCombobox.vue
@@ -1,15 +1,17 @@
 <template>
-  <select :class="classes">
-    <option value="select">Select a car...</option>
-    <option value="lamborghini">Lamborghini</option>
-    <option value="ferrari">Ferrari</option>
-    <option value="mitsubishi">Mitsubishi</option>
-    <option value="toyota">Toyota</option>
-    <option value="suzuki">Suzuki</option>
-    <option value="supra">Supra</option>
-    <option value="chevrolet">Chevrolet</option>
-    <option value="pulsar">Pulsar</option>
-  </select>
+  <div class="custom-select">
+    <select>
+      <option value="select">Placeholder</option>
+      <option value="lamborghini">Lamborghini</option>
+      <option value="ferrari">Ferrari</option>
+      <option value="mitsubishi">Mitsubishi</option>
+      <option value="toyota">Toyota</option>
+      <option value="suzuki">Suzuki</option>
+      <option value="supra">Supra</option>
+      <option value="chevrolet">Chevrolet</option>
+      <option value="pulsar">Pulsar</option>
+    </select>
+  </div>
 </template>
 
 <script>
@@ -35,6 +37,84 @@ export default {
       };
     },
   },
+
+  mounted() {
+    this.initComboboxElement();
+  },
+
+  methods: {
+    initComboboxElement() {
+      var x, i, j, l, ll, selElmnt, a, b, c;
+
+      function closeAllSelect(elmnt) {
+        var x, y, i, xl, yl, arrNo = [];
+        x = document.getElementsByClassName("select-items");
+        y = document.getElementsByClassName("select-selected");
+        xl = x.length;
+        yl = y.length;
+        for (i = 0; i < yl; i++) {
+          if (elmnt == y[i]) {
+            arrNo.push(i)
+          } else {
+            y[i].classList.remove("select-arrow-active");
+          }
+        }
+        for (i = 0; i < xl; i++) {
+          if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+          }
+        }
+      }
+
+      x = document.getElementsByClassName("custom-select");
+      l = x.length;
+
+      for (i = 0; i < l; i++) {
+        selElmnt = x[i].getElementsByTagName("select")[0];
+        ll = selElmnt.length;
+        a = document.createElement("DIV");
+        a.setAttribute("class", "select-selected");
+        a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+        x[i].appendChild(a);
+        b = document.createElement("DIV");
+        b.setAttribute("class", "select-items select-hide");
+        for (j = 1; j < ll; j++) {
+          c = document.createElement("DIV");
+          c.innerHTML = selElmnt.options[j].innerHTML;
+          c.addEventListener("click", function(e) {
+              var y, i, k, s, h, sl, yl;
+              s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+              sl = s.length;
+              h = this.parentNode.previousSibling;
+              for (i = 0; i < sl; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                  s.selectedIndex = i;
+                  h.innerHTML = this.innerHTML;
+                  y = this.parentNode.getElementsByClassName("same-as-selected");
+                  yl = y.length;
+                  for (k = 0; k < yl; k++) {
+                    y[k].removeAttribute("class");
+                  }
+                  this.setAttribute("class", "same-as-selected");
+                  break;
+                }
+              }
+              h.click();
+          });
+          b.appendChild(c);
+        }
+        x[i].appendChild(b);
+        a.addEventListener("click", function(e) {
+          e.stopPropagation();
+          closeAllSelect(this);
+          this.nextSibling.classList.toggle("select-hide");
+          this.classList.toggle("select-arrow-active");
+        });
+
+        document.addEventListener("click", closeAllSelect);
+      }
+    },
+  }
 };
 </script>
 
