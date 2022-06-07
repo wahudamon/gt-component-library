9e0ccb93aeb10714676afad3e732169b96a6c9ed
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-06-07 16:16:39 +0700

    feat: add icons on tab item section

--- src/components/GTTab/TabItem.vue
+++ src/components/GTTab/TabItem.vue
@@ -1,15 +1,23 @@
 <template>
   <li :data-tab-target="`#${tabId}`" :class="classes">
+    <GTIcon name="gt-icons-s" size="sm" />
     <p class="tab__text">{{ title }}</p>
+    <!-- <GTIcon v-if="icons" :name="icons" size="sm" />
+    <p v-else class="tab__text">{{ title }}</p> -->
+    <!-- <div class="tab__with-icon">
+      <GTIcon name="gt-icons-s" size="sm" />
+      <p class="tab__with-icon--text">{{ title }}</p>
+      <GTIcon name="gt-icons-s" size="sm" />
+    </div> -->
   </li>
 </template>
 
 <script>
-// import GTIcon from "../GTIcon/GTIcon.vue";
+import GTIcon from "../GTIcon/GTIcon.vue";
 
 export default {
   name: "TabItem",
-  // components: { GTIcon },
+  components: { GTIcon },
 
   props: {
     icons: {
