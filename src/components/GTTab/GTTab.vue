17ab6e767e1751079478690b0d83937753a395b1
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-06-07 10:14:24 +0700

    feat: create tab component base layout

--- src/components/GTTab/GTTab.vue
+++ src/components/GTTab/GTTab.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-    <TabReferences />
+  <!-- <TabReferences /> -->
+  <div class="tabs__container">
+    <ul class="tabs__field">
+      <li data-tab-target="#home" class="active tab">Home</li>
+      <li data-tab-target="#pricing" class="tab">Pricing</li>
+      <li data-tab-target="#about" class="tab">About</li>
+    </ul>
+
+    <div class="tab-content">
+      <div id="home" data-tab-content class="active">
+        <h1>Home</h1>
+        <p>This is the home.</p>
+      </div>
+      <div id="pricing" data-tab-content>
+        <h1>Home</h1>
+        <p>Some information on pricing.</p>
+      </div>
+      <div id="about" data-tab-content>
+        <h1>About</h1>
+        <p>Let me tell you about me.</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import TabReferences from "./references/TabReferences.vue";
-
 export default {
   name: "GTTab",
-  components: { TabReferences },
+
+  mounted() {
+    this.addTabsEvent();
+  },
+
+  methods: {
+    addTabsEvent() {
+      const tabs = document.querySelectorAll("[data-tab-target]");
+      const tabContents = document.querySelectorAll("[data-tab-content]");
+
+      tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+          const target = document.querySelector(tab.dataset.tabTarget);
+          tabContents.forEach((tabContent) => {
+            tabContent.classList.remove("active");
+          });
+          tabs.forEach((tab) => {
+            tab.classList.remove("active");
+          });
+          tab.classList.add("active");
+          target.classList.add("active");
+        });
+      });
+    },
+  },
 };
 </script>
 
--- src/components/GTTab/references/CodepenOne.vue
+++ src/components/GTTab/references/CodepenOne.vue
@@ -38,17 +38,7 @@ export default {
 </script>
 
 <style lang="scss">
-* {
-  box-sizing: border-box;
-}
-body,
-html {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-}
 body {
-  font-family: sans-serif;
   .container {
     width: 400px;
     position: absolute;
