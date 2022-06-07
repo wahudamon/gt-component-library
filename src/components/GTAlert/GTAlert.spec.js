318540bac30d9bce5c9ed1aac525bb73cec5321c
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-06-07 16:18:19 +0700

    refactor: remove commented syntax

--- src/components/GTAlert/GTAlert.spec.js
+++ src/components/GTAlert/GTAlert.spec.js
@@ -1,7 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import GTAlert from "./GTAlert.vue";
 
-// will be edited after creating checkbox component.
 describe("GTAlert.vue", () => {
   it("renders the props when passed", () => {
     const label = "Hello World!";
